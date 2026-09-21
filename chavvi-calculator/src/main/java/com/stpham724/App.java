9bd54bfab90aad54b4d16cdf60c58ba95d0f2483
Sang Pham <stpham724@gmail.com>
2026-09-21 12:22:46 -0700

    Added calculator menu

--- chavvi-calculator/src/main/java/com/stpham724/App.java
+++ chavvi-calculator/src/main/java/com/stpham724/App.java
@@ -1,13 +1,23 @@
 package com.stpham724;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        printMenu();
+    }
+
+    private static void printMenu() {
+        System.out.println("----------------------------------------------------");
+        System.out.println("Chavvi Calc");
+        System.out.println("----------------------------------------------------");
+        System.out.println("a       Enter a value for A");
+        System.out.println("b       Enter a value for B");
+        System.out.println("+       Add");
+        System.out.println("-       Subtract");
+        System.out.println("*       Multiply");
+        System.out.println("/       Divide");
+        System.out.println("c       Clear");
+        System.out.println("q       Quit");
+        System.out.println("----------------------------------------------------");
+        System.out.print("Enter a command: ");
     }
 }
